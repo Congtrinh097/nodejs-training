f55e40177b2ea693fb5ba30641867a18d3810ad4
congtrinh097 <congtrinh097>
2018-10-13 15:32:23 +0700

    Add UI

--- BaiTap-Njx03/routes/index.js
+++ BaiTap-Njx03/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET list pet */
+router.get('/list', function(req, res, next) {
+  res.render('list', { title: 'List your pets' });
+});
+
+/* GET list pet */
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Add  new pet' });
+});
+
+
+/* post login */
+router.post('/login', function(req, res, next) {
+  res.render('list', { title: 'List your pets' });
+});
+
 module.exports = router;
